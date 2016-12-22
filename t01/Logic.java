5b22196a4fd953b3fa8ef7c923b50ab1efebb5bf
veiden <flarix@yandex.ru>
2016-12-22 20:13:25 +0300

    Add files via upload
    First task. No package.

---
+++ t01/Logic.java
@@ -0,0 +1,7 @@
+public class Logic {
+	public String method(){
+		return "I am string in Logic.";
+	}
+
+
+}
---
+++ t01/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+
+	public static void main(String[] args) {
+		Logic logic = new Logic();
+		System.out.println(logic.method());
+	}
+}
